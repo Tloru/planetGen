fde5bb1f7abb909dcb01dc3cc3c156c1d66c7c91
IsaacImagine <isaacimagine@gmail.com>
2017-11-17 20:20:22 +0700

    finished "the brick"
    133-183

--- planetGen.py
+++ planetGen.py
@@ -99,43 +99,91 @@ change = 21000
 icosphere = genIco(13000, change)
 for j in range(5):
     change = change/2
-    icoshpere = subIco(icosphere, j+1, change)
+    #icoshpere = subIco(icosphere, j+1, change)
 print icosphere
 
 print
-print "ico build complete"
+print "full rand ico gen OFF (line 102)"
+print "full rand ico build complete"
 print "moving to triangle seeding"
 print
+
+#-----
 
 def seed(x,y=0):
     ra.seed(x)
     ra.jumpahead(y)
 
+#tri1
 #     1
 #   /  \
 #  /    \
 # /      \
 #2 ______ 3
 
+#tri2
+#2 ------ 3
+# \      /
+#  \    /
+#   \  /
+#    1
+
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 def genIcoTri(x, r, random):
     global alphabet
     tri = []
-    if x == 0:
+    if x == 0: #a block ---------- g
         for j in range(3):
             tri.append(r+(ra.uniform(0-random,random)))
-    if x >= 1 and x <= 3:
-        tri.append("a1")
-        tri.append(str(alphabet[x-1])+"3")
+    if x >= 1 and x <= 3: #bcd
+        tri.append("a[1]")
+        tri.append(str(alphabet[x-1])+"[3]")
         tri.append(r+(ra.uniform(0-random,random)))
-    if x == 4:
-        tri.append("a1")
-        tri.append("d2")
-        tri.append("a2")
-    if x >= 5 and x <= 15:
-        #tri.append()
+    if x == 4: #e
+        tri.append("a[1]")
+        tri.append("d[3]")
+        tri.append("a[2]")
+    if x == 5: #f block ---------- g
+        tri.append(r+(ra.uniform(0-random,random)))
+        tri.append("a[2]")
+        tri.append("a[3]")
+    if x >= 6 and x <= 8: #ghi
+        tri.append(r+(ra.uniform(0-random,random)))
+        tri.append(str(alphabet[x-6])+"[3]")
+        tri.append(str(alphabet[x-5])+"[3]")
+    if x == 9: #j
+        tri.append(r+(ra.uniform(0-random,random)))
+        tri.append("d[3]")
+        tri.append("a[2]")
+    if x == 10: #k block ---------- g
+        tri.append("a[3]")
+        tri.append("f[1]")
+        tri.append("g[1]")
+    if x >= 11 and x <= 13: #lmn
+        tri.append(str(alphabet[x-10])+"[3]")
+        tri.append(str(alphabet[x-5])+"[1]")
+        tri.append(str(alphabet[x-4])+"[1]")
+    if x == 14: #o
+        tri.append("a[2]")
+        tri.append("j[1]")
+        tri.append("f[1]")
+    if x == 15: #p block ---------- g
+        tri.append(r+(ra.uniform(0-random,random)))
+        tri.append("f[1]")
+        tri.append("g[1]")
+    if x >= 16 and x <= 18: #qrs
+        tri.append("p[1]")
+        tri.append(str(alphabet[x-10])+"[1]")
+        tri.append(str(alphabet[x-9])+"[1]")
+    if x == 19:
+        tri.append("p[1]")
+        tri.append("j[1]")
+        tri.append("f[1]")
     return tri
 
 for j in range(20):
-    print(genIcoTri(j, 5, 1))
+    print(str(alphabet[j])+str(genIcoTri(j, 1000, 0)))
+
+print
+print "sample planet generation at quality one (1) complete"
