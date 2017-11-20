1e5292b322adb7591de4e9bb5ad6d6b2cc5bc9c7
IsaacImagine <isaacimagine@gmail.com>
2017-11-20 18:49:25 +0700

    Update planetGen.py

--- planetGen.py
+++ planetGen.py
@@ -4,24 +4,28 @@
 
 import random as ra #importing random module
 
-pSize = 13000000 #planet size slightly larger than earth's
-pVari = 21000 #planet variation
-a = [pSize + ra.uniform(0 - pVari, pVari)] #top
-b = [pSize + ra.uniform(0 - pVari, pVari)] #front
-c = [pSize + ra.uniform(0 - pVari, pVari)] #left
-d = [pSize + ra.uniform(0 - pVari, pVari)] #back
-e = [pSize + ra.uniform(0 - pVari, pVari)] #right
-f = [pSize + ra.uniform(0 - pVari, pVari)] #bottom
+quality = 13 #sets the quality of the planet
+ra.seed(20040319) #sets the seed of the planet
+pSize = 6000000 #planet size slightly larger than earth's (radius)
+print 4*3.14*(pSize/1000)**2 #surface area of planet
 
-def divide(li): #this function divides a face of the planet into a smaller portion
+pVari = 21000 #planet variation | all units are in meters
+a = [pSize + ra.randint(0 - pVari, pVari)] #top
+b = [pSize + ra.randint(0 - pVari, pVari)] #front
+c = [pSize + ra.randint(0 - pVari, pVari)] #left
+d = [pSize + ra.randint(0 - pVari, pVari)] #back
+e = [pSize + ra.randint(0 - pVari, pVari)] #right
+f = [pSize + ra.randint(0 - pVari, pVari)] #bottom
+
+def divide(li, section = "All"): #this function divides a face of the planet into a smaller portion
     nLi = [] #stands for "new list"
     for j in range(len(li)): #repeats for each value in the list
         x = li[j] #takes an individual value from the list
         for j in range(4): #divides the list into 4 equal parts
-            nLi.append(x + ra.uniform(0 - pVari, pVari)) #adds value to nLi with some randomness
+            nLi.append(x + ra.randint(0 - pVari, pVari)) #adds value to nLi with some randomness
     return nLi #returns the subdivided list
 
-for j in range(26): #repeats the amout nescary for each value to represent roughly a meter of space (26 times)
+for j in range(quality): #repeats the amout nescary for each value to represent roughly a meter of space (13 times)
     pVari = pVari / 2 #makes things less random as they get smaller
     a = divide(a) #divides the top
     b = divide(b) #divides the front
@@ -29,6 +33,7 @@ for j in range(26): #repeats the amout nescary for each value to represent rough
     d = divide(d) #divides the back
     e = divide(e) #divides the right
     f = divide(f) #divides the bottom
+    print j #displays what division step it's currently on
 
 print a #displays the top surface values
 print b # ^ front
@@ -36,4 +41,3 @@ print c # ^ left
 print d # ^ back
 print e # ^ right
 print f # ^ bottom
-
