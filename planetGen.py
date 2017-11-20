f8783b8f6a3c2fc4e8afb198fa7fde3b9d1866d8
IsaacImagine <isaacimagine@gmail.com>
2017-11-20 17:42:47 +0700

    Update planetGen.py

--- planetGen.py
+++ planetGen.py
@@ -1,111 +1,38 @@
-import random as ra
-
+#-----
 #planetGen
+#-----
 
+import random as ra #importing random module
 
-def seed(x,y=0):
-    ra.seed(x)
-    ra.jumpahead(y)
+pSize = 13000000 #planet size slightly larger than earth's
+pVari = 21000 #planet variation
+a = [pSize + ra.uniform(0 - pVari, pVari)] #top
+b = [pSize + ra.uniform(0 - pVari, pVari)] #front
+c = [pSize + ra.uniform(0 - pVari, pVari)] #left
+d = [pSize + ra.uniform(0 - pVari, pVari)] #back
+e = [pSize + ra.uniform(0 - pVari, pVari)] #right
+f = [pSize + ra.uniform(0 - pVari, pVari)] #bottom
 
-#tri1
-#    1x
-#   /  \
-#  /    \
-# /      \
-#2y ____ 3z
+def divide(li): #this function divides a face of the planet into a smaller portion
+    nLi = [] #stands for "new list"
+    for j in range(len(li)): #repeats for each value in the list
+        x = li[j] #takes an individual value from the list
+        for j in range(4): #divides the list into 4 equal parts
+            nLi.append(x + ra.uniform(0 - pVari, pVari)) #adds value to nLi with some randomness
+    return nLi #returns the subdivided list
 
-#tri2
-#2y ---- 3z
-# \      /
-#  \    /
-#   \  /
-#    x
+for j in range(26): #repeats the amout nescary for each value to represent roughly a meter of space (26 times)
+    pVari = pVari / 2 #makes things less random as they get smaller
+    a = divide(a) #divides the top
+    b = divide(b) #divides the front
+    c = divide(c) #divides the left
+    d = divide(d) #divides the back
+    e = divide(e) #divides the right
+    f = divide(f) #divides the bottom
 
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-def genIcoTri(x, r, random):
-    #each triangle covers around 40 million square kilometers.
-    global alphabet
-    tri = []
-    if x == 0: #a block ---------- g
-        for j in range(3):
-            tri.append(r+(ra.uniform(0-random,random)))
-    if x >= 1 and x <= 3: #bcd
-        tri.append("a[1]")
-        tri.append(str(alphabet[x-1])+"[3]")
-        tri.append(r+(ra.uniform(0-random,random)))
-    if x == 4: #e
-        tri.append("a[1]")
-        tri.append("d[3]")
-        tri.append("a[2]")
-    if x == 5: #f block ---------- g
-        tri.append(r+(ra.uniform(0-random,random)))
-        tri.append("a[2]")
-        tri.append("a[3]")
-    if x >= 6 and x <= 8: #ghi
-        tri.append(r+(ra.uniform(0-random,random)))
-        tri.append(str(alphabet[x-6])+"[3]")
-        tri.append(str(alphabet[x-5])+"[3]")
-    if x == 9: #j
-        tri.append(r+(ra.uniform(0-random,random)))
-        tri.append("d[3]")
-        tri.append("a[2]")
-    if x == 10: #k block ---------- g
-        tri.append("a[3]")
-        tri.append("f[1]")
-        tri.append("g[1]")
-    if x >= 11 and x <= 13: #lmn
-        tri.append(str(alphabet[x-10])+"[3]")
-        tri.append(str(alphabet[x-5])+"[1]")
-        tri.append(str(alphabet[x-4])+"[1]")
-    if x == 14: #o
-        tri.append("a[2]")
-        tri.append("j[1]")
-        tri.append("f[1]")
-    if x == 15: #p block ---------- g
-        tri.append(r+(ra.uniform(0-random,random)))
-        tri.append("f[1]")
-        tri.append("g[1]")
-    if x >= 16 and x <= 18: #qrs
-        tri.append("p[1]")
-        tri.append(str(alphabet[x-10])+"[1]")
-        tri.append(str(alphabet[x-9])+"[1]")
-    if x == 19:
-        tri.append("p[1]")
-        tri.append("j[1]")
-        tri.append("f[1]")
-    return tri
-
-def genPlanet(r, random):
-    dic = {}
-    for j in range(20):
-        dic[str(alphabet[j])] = genIcoTri(j, r, random)
-        dic['quality'] = 1
-    return dic
-
-def getTri(dic, t):
-    z = dic
-    w = []
-    for j in range(3):
-        x = t[j]
-        if isinstance (x, str):
-            y = x[0]
-            y = z[str(y)]
-            x = y[int(x[2])-1]
-        w.append(x)
-    print w
-    return w
-
-def subTri(dic, quality):
-    for j in range(20):
-        x = getTri(dic, dic[str(alphabet[j])])
-
-
-change = 20.48
-planet = genPlanet(1000, 0)
-print planet
-
-print
-print "sample planet generation at quality one (1) complete"
-print
-#planet = subTri(planet)
+print a #displays the top surface values
+print b # ^ front
+print c # ^ left
+print d # ^ back
+print e # ^ right
+print f # ^ bottom
