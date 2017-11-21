e3289b242d331e7f74b24abee309baafe9497f2c
IsaacImagine <isaacimagine@gmail.com>
2017-11-21 15:55:53 +0700

    Update planetGen.py

--- planetGen.py
+++ planetGen.py
@@ -4,28 +4,36 @@
 
 import random as ra #importing random module
 
-quality = 13 #sets the quality of the planet
 ra.seed(20040319) #sets the seed of the planet
-pSize = 6000000 #planet size slightly larger than earth's (radius)
-print 4*3.14*(pSize/1000)**2 #surface area of planet
+pSize = 6000000 #planet size slightly smaller than earth's (radius)
+pArea =  4*3.14*(pSize/1000)**2 #surface area of planet in km^2
+#
+quality = 13 #sets the quality of the planet
 
-pVari = 21000 #planet variation | all units are in meters
-a = [pSize + ra.randint(0 - pVari, pVari)] #top
-b = [pSize + ra.randint(0 - pVari, pVari)] #front
-c = [pSize + ra.randint(0 - pVari, pVari)] #left
-d = [pSize + ra.randint(0 - pVari, pVari)] #back
-e = [pSize + ra.randint(0 - pVari, pVari)] #right
-f = [pSize + ra.randint(0 - pVari, pVari)] #bottom
+pVari = 21000 #planet variation (ie. maximum mountain height) | all units are in meters
+a = ["1", pSize + ra.randint(0 - pVari, pVari)] #top
+b = ["1", pSize + ra.randint(0 - pVari, pVari)] #front
+c = ["1", pSize + ra.randint(0 - pVari, pVari)] #left
+d = ["1", pSize + ra.randint(0 - pVari, pVari)] #back
+e = ["1", pSize + ra.randint(0 - pVari, pVari)] #right
+f = ["1", pSize + ra.randint(0 - pVari, pVari)] #bottom
 
-def divide(li, section = "All"): #this function divides a face of the planet into a smaller portion
+def divide(li, section = "None"): #this function divides a face of the planet into a smaller portion
     nLi = [] #stands for "new list"
-    for j in range(len(li)): #repeats for each value in the list
-        x = li[j] #takes an individual value from the list
-        for j in range(4): #divides the list into 4 equal parts
-            nLi.append(x + ra.randint(0 - pVari, pVari)) #adds value to nLi with some randomness
+    if section == "None": #divides one part of the world by one step, no matter the quality before
+        for j in range(len(li)): #repeats for each value in the list
+            x = li[j] #takes an individual value from the list
+            if str(type(x)) == "<type 'str'>":
+                y =
+                nLi.append(str(1+int(x)))
+            else:
+                for j in range(4): #divides the list into 4 equal parts
+                    nLi.append(x + ra.randint(0 - pVari, pVari)) #adds value to nLi with some randomness
+    else:
+        nli = []
     return nLi #returns the subdivided list
 
-for j in range(quality): #repeats the amout nescary for each value to represent roughly a meter of space (13 times)
+for j in range(quality): #repeats the amout nescary for each value to represent roughly a meter of space (15 times)
     pVari = pVari / 3 #makes things less random as they get smaller
     a = divide(a) #divides the top
     b = divide(b) #divides the front
