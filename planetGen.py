28501252706f1feea24f36aea60d96eddf06bb86
IsaacImagine <isaacimagine@gmail.com>
2017-11-22 06:00:42 +0700

    Update planetGen.py

--- planetGen.py
+++ planetGen.py
@@ -7,8 +7,21 @@ import random as ra #importing random module
 ra.seed(20040319) #sets the seed of the planet
 pSize = 6000000 #planet size slightly smaller than earth's (radius)
 pArea =  4*3.14*(pSize/1000)**2 #surface area of planet in km^2
-#
-quality = 13 #sets the quality of the planet
+
+def calcQuality(sA, qS): #calculates the quality level needed to achieve a certain distance between nodes
+    j = -1 #j is the amount of quality needed
+    x = sA/6 #divided the total surface area by 6, the size of each face on the cube
+    while True: #repeat until loop
+        j+=1 #counter goes up
+        print str(x)+" "+str(j) #displays the current tile size and quality
+        if x <= qS: #breaks loop when conditions are met
+            return j-1 #returns the counter
+        else: #else
+            x = int(x/4) #divides the surface further into 4 more pieces
+
+quality = calcQuality(pArea, 1000) #sets the quality of the planet
+print(pArea)
+print(quality)
 
 pVari = 21000 #planet variation (ie. maximum mountain height) | all units are in meters
 a = ["1", pSize + ra.randint(0 - pVari, pVari)] #top
@@ -24,10 +37,10 @@ def divide(li, section = "None"): #this function divides a face of the planet in
         for j in range(len(li)): #repeats for each value in the list
             x = li[j] #takes an individual value from the list
             if str(type(x)) == "<type 'str'>":
-                y =
-                nLi.append(str(1+int(x)))
+                y = str(1+int(x))
             else:
                 for j in range(4): #divides the list into 4 equal parts
+                    nLi.append(y)
                     nLi.append(x + ra.randint(0 - pVari, pVari)) #adds value to nLi with some randomness
     else:
         nli = []
